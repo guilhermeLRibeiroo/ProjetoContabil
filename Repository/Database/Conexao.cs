820daade77e6c7640466df5c0eadda9d5f8194b9
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-07-01 20:14:35 -0300

    Adicionado classe de Conexao para facilitar SqlConnection.

---
+++ Repository/Database/Conexao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Database
+{
+    public class Conexao
+    {
+        public static SqlCommand AbrirConexao()
+        {
+            SqlConnection conexao = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString());
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            return comando;
+        }
+    }
+}
