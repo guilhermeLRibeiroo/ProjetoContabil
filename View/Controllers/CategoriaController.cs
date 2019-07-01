3a2ecbb5145b15b67b3398eef91c761f0b3a33c9
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-07-01 20:17:44 -0300

    Adicionado arquivos(tipo bootstrap e font.awesome) no projeto. Adicionado CategoriaController e View Index de categorias

---
+++ View/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace View.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        CategoriaRepository repository;
+
+        public CategoriaController()
+        {
+            repository = new CategoriaRepository();
+        }
+
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            ViewBag.Categorias = repository.ObterTodos("");
+            return View();
+        }
+    }
+}
