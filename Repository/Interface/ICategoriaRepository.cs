5bbda57810935855dbdf8b66be02d5ec613f1b0f
Guilherme <48655138+guilhermeLRibeiroo@users.noreply.github.com>
2019-07-01 20:14:35 -0300

    Adicionado interface do repository de categoria

---
+++ Repository/Interface/ICategoriaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    interface ICategoriaRepository
+    {
+        int Inserir(Categoria categoria);
+
+        bool Atualizar(Categoria categoria);
+
+        bool Apagar(int id);
+
+        Categoria ObterPeloId(int id);
+
+        List<Categoria> ObterTodos(string busca);
+    }
+}
