014eb95d636b651e7f1a0e5d41105d5ea5b9031f
maxjulianog <maxjulianog@gmail.com>
2019-07-02 14:47:33 -0300

    Adicionado classe Contabilidade em Model, Adicionado as Views Editar, Cadastrar e Index e as funcionalidades no ContabilidadeController

--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Contabilidade
+    public class Usuario
     {
         public int Id;
-        public string Nome;
+        public string Login;
+        public string Senha;
+        public DateTime Data_Nascimento;
+        public int IdContabilidade;
     }
 }
